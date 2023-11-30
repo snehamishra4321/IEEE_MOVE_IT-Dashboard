b8e7e5ebf443407b3a8279fc3828b0a76000f95c
Udbhav Srivastava <118310067+UdbhavSrivastava@users.noreply.github.com>
2023-11-30 13:17:36 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -85,9 +85,10 @@ data_points = generate_random_points(min_latitude, max_latitude, min_longitude, 
 # Create a DataFrame from the generated random points
 data_points = pd.DataFrame(data_points, columns=['Latitude', 'Longitude'])
 
-score_data = pd.read_csv('./data/filtered_new_point_data_v1.csv')
+#score_data = pd.read_csv('c:/Users/Christopher/Downloads/filtered_new_point_data_v1.csv')
 
-tower_df = pd.read_csv('./data/Cellular_Towers_V1.csv')
+score_data = pd.read_csv('./data/scores.csv')
+tower_df = pd.read_csv('Cellular_Towers_V1.csv')
 filtered_tower_df = tower_df[(tower_df['X']>min_longitude) & (tower_df['X']<max_longitude) & (
     tower_df['Y']>min_latitude) & (tower_df['Y']<max_latitude) ]
 
@@ -100,24 +101,7 @@ truck_points = pd.DataFrame(truckdata)
 # Create a map centered around the first location in your data
 m = folium.Map(location=[data_points['Latitude'].iloc[0], data_points['Longitude'].iloc[0]], zoom_start=12,tiles="OpenStreetMap",control_scale =True)
 
-
-#score_data = score_data.head(100)
-# lgd_txt = '<span style="color: {col};">Score</span>'
-# color = 'red'
-# # Add markers for each data point
-# data = folium.FeatureGroup(lgd_txt.format( txt= color+' egg', col= "#055C9D") ).add_to(m)
-
-# for index, row in score_data.iterrows():
-#     point = loads(row['geometry'])
-
-#     # Extract latitude and longitude
-#     lat, lon = point.y, point.x
-#     tooltip = f"Score: {row['Score']}"
-#     folium.Marker([lat,lon], opacity=0.7,
-#                   icon = BeautifyIcon(
-#                 prefix = "fa", icon="star", border_color="green", text_color="green"), tooltip = tooltip #,  icon_size=(5,5)
-#                   ).add_to(data)
-    
+   
 
 # Define a function to get a color based on the score
 def get_color(score):
@@ -128,54 +112,41 @@ def get_color(score):
     g = int(255 * (normalized_score**2))
     return f'rgb({r},{g},0)'
 
-# for index, row in score_data.iterrows():
-#     point = loads(row['geometry'])
-#     lat, lon = point.y, point.x
+ 
 
-#     # Add a tooltip with the score and dynamic color
-#     tooltip = f"Score: {row['Score']}"
-#     color = get_color(row['Score'])
-
-#     folium.Marker([lat, lon], opacity=0.7,
-#                   icon=folium.Icon(color=color, icon='star', prefix='fa'),
-#                   tooltip=tooltip,
-#                   popup=f"<b>{tooltip}</b>",
-#                   ).add_to(data)
-    
-
-lgd_txt = '<span style="color: {col};">Crowd</span>'
+# lgd_txt = '<span style="color: {col};">Crowd</span>'
 color = 'red'
-# Add markers for each data point
-data = folium.FeatureGroup(lgd_txt.format( txt= color+' egg', col= "#055C9D"),show = False ).add_to(m)
-for index, row in data_points.iterrows():
-    folium.Marker([row['Latitude'], row['Longitude']], opacity=0.7,
-                  icon = BeautifyIcon(
-                prefix = "fa", icon="user", border_color="blue", text_color="blue" #,  icon_size=(5,5)
-                  )).add_to(data)
+# # Add markers for each data point
+# data = folium.FeatureGroup(lgd_txt.format( txt= color+' egg', col= "#055C9D"),show = False ).add_to(m)
+# for index, row in data_points.iterrows():
+#     folium.Marker([row['Latitude'], row['Longitude']], opacity=0.7,
+#                   icon = BeautifyIcon(
+#                 prefix = "fa", icon="user", border_color="blue", text_color="blue" #,  icon_size=(5,5)
+#                   )).add_to(data)
 
-lgd_txt = '<span style="color: {col};">Towers</span>'
-tower = folium.FeatureGroup(lgd_txt.format( txt= color+' egg', col= "#922B21"),show = False).add_to(m)
-for ind, row in filtered_tower_df.iterrows():
-    folium.Marker(location = [row['latdec'], row['londec']],icon = BeautifyIcon(
-    prefix = "fa", icon="signal", border_color="#922B21", text_color="#922B21", text_size = 100,  icon_size=(50,50), inner_icon_style="font-size:24px;padding-top:5px;",
-)).add_to(tower)
+# lgd_txt = '<span style="color: {col};">Towers</span>'
+# tower = folium.FeatureGroup(lgd_txt.format( txt= color+' egg', col= "#922B21"),show = False).add_to(m)
+# for ind, row in filtered_tower_df.iterrows():
+#     folium.Marker(location = [row['latdec'], row['londec']],icon = BeautifyIcon(
+#     prefix = "fa", icon="signal", border_color="#922B21", text_color="#922B21", text_size = 100,  icon_size=(50,50), inner_icon_style="font-size:24px;padding-top:5px;",
+# )).add_to(tower)
 
-lgd_txt = '<span style="color: {col};">Truck</span>'
-truck = folium.FeatureGroup(lgd_txt.format( txt= color+' egg', col= "#4CBB17"),show = False).add_to(m)
-for index, row in truck_points.iterrows():
-    folium.Marker(location = [row['Latitude'], row['Longitude']],icon = BeautifyIcon(
-    icon="truck", border_color="#4CBB17", text_color="#4CBB17", icon_shape=None, icon_size=(60,60), inner_icon_style="font-size:35px;padding-top:5px;",
-)).add_to(truck)
+# lgd_txt = '<span style="color: {col};">Truck</span>'
+# truck = folium.FeatureGroup(lgd_txt.format( txt= color+' egg', col= "#4CBB17"),show = False).add_to(m)
+# for index, row in truck_points.iterrows():
+#     folium.Marker(location = [row['Latitude'], row['Longitude']],icon = BeautifyIcon(
+#     icon="truck", border_color="#4CBB17", text_color="#4CBB17", icon_shape=None, icon_size=(60,60), inner_icon_style="font-size:35px;padding-top:5px;",
+# )).add_to(truck)
 
 # Create a HeatMap layer using population data
-lgd_txt = '<span style="color: {col};">Crowd Heatmap</span>'
-heat_data = [[row['Latitude'], row['Longitude']] for index, row in data_points.iterrows()]
-HeatMap(heat_data, min_opacity=0.1, blur = 30, name= lgd_txt.format( txt= color+' egg', col= "#055C9D"),show = False ).add_to(m)
+# lgd_txt = '<span style="color: {col};">Crowd Heatmap</span>'
+# heat_data = [[row['Latitude'], row['Longitude']] for index, row in data_points.iterrows()]
+# HeatMap(heat_data, min_opacity=0.1, blur = 30, name= lgd_txt.format( txt= color+' egg', col= "#055C9D"),show = False ).add_to(m)
 
 # Create a HeatMap layer using tower data
-lgd_txt = '<span style="color: {col};">Signal Heatmap</span>'
-heatmap_data = [[row['latdec'], row['londec']] for ind, row in filtered_tower_df.iterrows()]
-HeatMap(heatmap_data, min_opacity=0.1, blur = 30, radius = 100,name= lgd_txt.format( txt= color+' egg', col= "#055C9D"), show = False).add_to(m)
+# lgd_txt = '<span style="color: {col};">Signal Heatmap</span>'
+# heatmap_data = [[row['latdec'], row['londec']] for ind, row in filtered_tower_df.iterrows()]
+# HeatMap(heatmap_data, min_opacity=0.1, blur = 30, radius = 100,name= lgd_txt.format( txt= color+' egg', col= "#055C9D"), show = False).add_to(m)
 
 def fit_bounds(points, m):
     sw = points[['Latitude', 'Longitude']].min().values.tolist()
@@ -200,7 +171,8 @@ gdf_read = gpd.read_file(fp,
                     GEOM_POSSIBLE_NAMES="geometry", 
                     KEEP_GEOM_COLUMNS="NO")
 
-gdf_read['GEOID'] = gdf_read['GEOID'].apply(lambda x:int(x))
+print(gdf_read.info())
+#gdf_read['GEOID'] = gdf_read['GEOID'].apply(lambda x:int(x))
 gdf_read['GEOID_12'] = gdf_read['GEOID_12'].apply(lambda x:int(x))
 gdf_read['TotPop'] = gdf_read['TotPop'].apply(lambda x:int(x))
 gdf_read['D1B'] = gdf_read['D1B'].apply(lambda x:float(x))
@@ -250,9 +222,9 @@ power_outage = gdf.set_index("GEOID")["power"]
 
 print(gdf)
 
-legend_walkind = colormap_walkind.to_step(index=[0, 1, 2, 3, 4, 5, 6, 7, 8])
-legend_totpop = colormap_totpop.to_step(index=[0, 1, 2, 3, 4, 5])
-legend_power = colormap_power.to_step(index=[0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1])
+# legend_walkind = colormap_walkind.to_step(index=[0, 1, 2, 3, 4, 5, 6, 7, 8])
+# legend_totpop = colormap_totpop.to_step(index=[0, 1, 2, 3, 4, 5])
+# legend_power = colormap_power.to_step(index=[0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1])
 
 
 folium.GeoJson(
@@ -267,8 +239,8 @@ folium.GeoJson(
     }, 
 ).add_to(m)
 
-colormap_walkind.caption = "Walkability Index color scale"
-colormap_walkind.add_to(m)
+# colormap_walkind.caption = "Walkability Index color scale"
+# colormap_walkind.add_to(m)
 
 
 folium.GeoJson(
@@ -315,49 +287,6 @@ folium.GeoJson(
     show = False,
 ).add_to(m)
 
-
-# Add GeoJson layers
-# geojson_walkability = folium.GeoJson(
-#     gdf,
-#     name="Walkability Index",
-#     style_function=lambda feature: {
-#         "fillColor": colormap_walkind(walk_ind_dict[feature["properties"]["GEOID"]]),
-#         "color": "black",
-#         "weight": 1,
-#         "dashArray": "5, 5",
-#         "fillOpacity": 0.5,
-#     },
-# )
-
-# geojson_total_population = folium.GeoJson(
-#     gdf,
-#     name="Total Population",
-#     style_function=lambda feature: {
-#         "fillColor": colormap_totpop(tot_pop_dict[feature["properties"]["GEOID"]]),
-#         "color": "black",
-#         "weight": 1,
-#         "dashArray": "5, 5",
-#         "fillOpacity": 0.5,
-#     },
-# )
-
-# geojson_power = folium.GeoJson(
-#     gdf,
-#     name="Power",
-#     style_function=lambda feature: {
-#         "fillColor": colormap_power(power_outage[feature['properties']['GEOID']]),
-#         "color": "blue",
-#         "weight": 1,
-#         "dashArray": "5, 5",
-#         "fillOpacity": 0.5,
-#     },
-# )
-
-# Add GeoJson layers to the map
-# geojson_walkability.add_to(m)
-# geojson_total_population.add_to(m)
-# geojson_power.add_to(m)
-
 # colormap_power.caption = "Power color scale"
 # colormap_power.add_to(m)
 
@@ -365,25 +294,7 @@ folium.GeoJson(
 #colormap_totpop.add_to(m)
 #colormap_power.add_to(m)
 
-# gdf_score = gpd.GeoDataFrame(score_data[['STATEFP_x', 'COUNTYFP_x', 'TRACTCE_x', 'BLKGRPCE_x',  'GEOID', 'geometry', 'TotPop', 'D1B', 'NatWalkInd','GEOID_12','Score']])
 
-
-# colors = ["orange", "yellow", "green", "blue"]
-# Score = gdf_score.Score.unique().tolist()
-
-# folium.GeoJson(
-#     gdf_score,
-#     name="Score Points",
-#     marker=folium.Circle(radius=4, fill_color="orange", fill_opacity=0.4, color="black", weight=1),
-#     tooltip=folium.GeoJsonTooltip(fields=["name", "line", "notes"]),
-#     popup=folium.GeoJsonPopup(fields=["name", "line", "href", "notes"]),
-#     style_function=lambda x: {
-#         "fillColor": colors[x['properties']['Score']],
-#         "radius": (x['properties']['Score'])*30,
-#     },
-#     highlight_function=lambda x: {"fillOpacity": 0.8},
-#     zoom_on_click=True,
-# ).add_to(m)
 
 icon_create_function = """\
 function(cluster) {
@@ -396,15 +307,9 @@ function(cluster) {
 
 
 from shapely.geometry import Point
+from scipy.stats import boxcox
 
-# print(score_data.head())
-# # score_data['Points'] = score_data['Points'].apply(lambda point: Point(point))
-# score_data['Points'] = [Point(coords) for coords in score_data['Points']]
-# # Extract latitude and longitude from each point
-# print(score_data.head())
-# lats = score_data['Points'].apply(lambda point: point.y)
-# lons = score_data['Points'].apply(lambda point: point.x)
-
+score_data['boxcox_score'], _ = boxcox(score_data['score'] + 1)  # Adding 1 to avoid issues with zero values
 
 score_data[['longitude', 'latitude']] = score_data['Points'].str.strip('()').str.split(', ', expand=True)
 
@@ -428,30 +333,61 @@ marker_cluster = MarkerCluster(
 )
 
 for i, row in score_data.iterrows():
-    tooltip = f"Score: {row['score']}"
+    tooltip = f"Score: {row['boxcox_score']}"
     folium.Marker(
         location=(row['latitude'], row['longitude']),
         tooltip=tooltip,
-        icon=None,  # Assuming you have a function to create custom icons
+        icon=None,  
     ).add_to(marker_cluster)
 
 
 marker_cluster.add_to(m)
 
+score_data = score_data.sample(50)
+# Extract latitude, longitude, and score from your DataFrame
+locations = score_data[['latitude', 'longitude', 'boxcox_score']].values
+
+from folium import LinearColormap
+# Define a color gradient based on score using LinearColormap
+color_grad = LinearColormap(['cyan', 'lime', 'yellow', 'black'], vmin=0, vmax=score_data['boxcox_score'])
+
+
+# color_gradient = {
+#      0.0: 'cyan',
+#     0.2: 'lime',
+#     0.7: 'yellow',
+#     #0.6: '#645099',
+#     #0.8: '#493a70',
+#     1.0: 'black'
+# }
+
+color_gradient = {
+    0.0:"green",
+    0.2:"cyan",
+    0.7:"green",
+    1:"red"
+}
+color_grad.caption = "Score color scale"
+color_grad.add_to(m)
+
+
+lgd_txt = '<span>Score Heatmap</span>'
+# Create a HeatMap layer with the color gradient
+HeatMap(locations, name= lgd_txt.format( txt= color+' egg'),show= True).add_to(m)
+
+marker_cluster.add_to(m)
 
 m.fit_bounds(m.get_bounds())
 
 folium.LayerControl().add_to(m)
 folium.map.LayerControl('topleft', collapsed= False).add_to(m)
 
-# layer_control = folium.LayerControl(collapsed=False)
-# layer_control.add_child(geojson_walkability, name="Walkability Index")
-# layer_control.add_child(geojson_total_population, name="Total Population")
-# layer_control.add_child(geojson_power, name="Power")
-
-# layer_control.add_to(m)
+import streamlit as st
 from streamlit_folium import st_folium
 from folium.plugins import Fullscreen
+
+st.title('Move-It Truck Dashboard')
+#st.markdown("# Move-It Truck Dashboard")
 
 Fullscreen(position="topleft").add_to(m)
 # st_data = st_folium(m, width=725)
